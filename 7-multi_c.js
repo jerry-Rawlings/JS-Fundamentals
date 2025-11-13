0bf7aaab2386aea2952a8c1e34d9333be0fd15d2
Jerry Rawlings <jerryrawlings73@gmail.com>
2025-11-13 15:58:10 +0000

    c push

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,11 +1,11 @@
 // Get the first argument
 const arg = process.argv[2];
 
-// Convert it to an integer
+// Convert to integer
 const x = parseInt(arg, 10);
 
-// Check if the conversion is valid and positive
-if (isNaN(x) || x <= 0) {
+// If argument is missing or not a number
+if (isNaN(x)) {
   console.log("Missing number of occurrences");
 } else {
   let i = 0;
